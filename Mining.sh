afcb606826085344fa17be86978fc14d9d6c3306
Ethonwu <a5351998@gmail.com>
2018-08-23 22:18:18 +0800

    Modify Mining.sh and write sth from auto.py

--- Mining.sh
+++ Mining.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
-name="Jan"
-for i in `seq 1 27`
+#name="Jan"
+mkdir final_result
+path=`pwd`
+min_sup="0.5"
+echo 
+for i in ` ls | grep .txt`
 do
-  filename=$name$i".txt"
+  filename=$i
   outputfile="output_"$filename
   tablefile="Table_"$outputfile
   final_result="final_"$filename
 
-  python2.7 pre.py -f $filename -o $outputfile
-  java -jar ../spmf.jar run Apriori $outputfile ddd 0.5
+  echo $outputfile
+
+  python2.7 ~/packet/script/pre.py -f $filename -o $outputfile
+  java -jar ~/packet/script/spmf.jar run Apriori $outputfile ddd $min_sup
   sed -i 's/ /,/g' ddd
   sed -i 's/,,#SUP:,/ /' ddd
   sed -i 's/,#SUP:,/ /' ddd
-  python2.7 Value_to_Item.py -f ddd -t $tablefile > $final_result
+  python2.7 ~/packet/script/Value_to_Item.py -f ddd -t $tablefile > final_$min_sup_result/$final_result
   rm ddd
 
 
 
 done
+
